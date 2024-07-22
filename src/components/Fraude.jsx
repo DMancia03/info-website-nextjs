b41efb4cb2d3d20024730562475f14e27d4204b5
DMancia03 <diego.mancia.03@gmail.com>
2024-07-21 19:20:28 -0600

    feature: Initial website design

---
+++ src/components/Fraude.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import FraudeCaracteristica from "./FraudeCaracteristica";
+import FraudeStyles from '../styles/fraude.module.css'
+
+const Fraude = ({fraude}) => {
+    return (
+        <>
+            <div className={FraudeStyles.fraudeCard}>
+                <div className={FraudeStyles.fraudeCard_Title}>
+                    <h2>{fraude.titulo}</h2>
+                </div>
+                <div className={FraudeStyles.fraudeCard_Description}>
+                    <h4>Descripción:</h4>
+                    <p>{fraude.descripcion}</p>
+                </div>
+                <div className={FraudeStyles.fraudeCard_Feature}>
+                    <h4>Características:</h4>
+                    {Object.entries(fraude.caracteristicas).map((value, index) => (<FraudeCaracteristica c={value}/>))}
+                </div>
+                <div className={FraudeStyles.fraudeCard_Image}>
+                    <Image 
+                        src={fraude.imagen_de_referencia}
+                        width={200}
+                        height={200}
+                        alt={fraude.titulo}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Fraude
---
+++ src/components/FraudeCaracteristica.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FraudeCaracteristica = ({c}) => {
+    return (
+        <>
+            <p>{c[1]}</p>
+        </>
+    )
+}
+
+export default FraudeCaracteristica
